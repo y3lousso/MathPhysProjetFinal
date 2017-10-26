8c632c0528f5bec5396219f8fd9b0df07f652c5d
y3lousso <y3lousso@gmail.com>
2017-10-26 17:14:25 -0400

    Maths Utility : Vector3, Mat3x3, Mat4x4 GameEngine (pour que je puisse tester ma méthode)

---
+++ Assets/Script/GameEngine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MathsPhys
+{
+    public class GameEngine : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            float[,] mat = new float[,]{
+                { 5, 5, 5, 5 },
+                { 5, 5, 5, 5 },
+                { 5, 5, 5, 5 },
+                { 5, 5, 5, 5 },
+            };
+            Matrix4x4 matCube = new Matrix4x4(mat);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
