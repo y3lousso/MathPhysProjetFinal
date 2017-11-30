4d2c50b422496bd670548dec960157d81f8e8f7f
saarg <milsonneau.jean@free.fr>
2017-11-30 11:21:16 -0500

    mathutils

---
+++ Assets/MathsUtility/Matrix4x4.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MathsPhys
+{
+    public class Matrix4x4 : Matrix
+    {
+        public static Matrix4x4 I = NewI();
+
+        public Matrix4x4()
+            : base(4, 4)
+        {
+        }
+
+        public Matrix4x4(float[,] matrix)
+            : base(matrix)
+        {
+            if (rows != 4 || cols != 4)
+            {
+                // throw new ArgumentException();
+                Debug.LogError("Error");
+            }
+        }
+
+        public static Matrix4x4 NewI()
+        {
+            return new Matrix4x4(new float[,] {
+        { 1.0f, 0.0f, 0.0f, 0.0f },
+        { 0.0f, 1.0f, 0.0f, 0.0f },
+        { 0.0f, 0.0f, 1.0f, 0.0f },
+        { 0.0f, 0.0f, 0.0f, 1.0f } });
+        }
+
+        public static Vector3 operator *(Matrix4x4 Matrix4x4, Vector3 v)
+        {
+            float[,] m = Matrix4x4.matrix;
+            float w = m[3, 0] * v.x + m[3, 1] * v.y + m[3, 2] * v.z + m[3, 3];
+            return new Vector3(
+                (m[0, 0] * v.x + m[0, 1] * v.y + m[0, 2] * v.z + m[0, 3]) / w,
+                (m[1, 0] * v.x + m[1, 1] * v.y + m[1, 2] * v.z + m[1, 3]) / w,
+                (m[2, 0] * v.x + m[2, 1] * v.y + m[2, 2] * v.z + m[2, 3]) / w
+                );
+        }
+
+        public static Matrix4x4 operator *(Matrix4x4 mat1, Matrix4x4 mat2)
+        {
+            float[,] m1 = mat1.matrix;
+            float[,] m2 = mat2.matrix;
+            float[,] m3 = new float[4, 4];
+            m3[0, 0] = m1[0, 0] * m2[0, 0] + m1[0, 1] * m2[1, 0] + m1[0, 2] * m2[2, 0] + m1[0, 3] * m2[3, 0];
+            m3[0, 1] = m1[0, 0] * m2[0, 1] + m1[0, 1] * m2[1, 1] + m1[0, 2] * m2[2, 1] + m1[0, 3] * m2[3, 1];
+            m3[0, 2] = m1[0, 0] * m2[0, 2] + m1[0, 1] * m2[1, 2] + m1[0, 2] * m2[2, 2] + m1[0, 3] * m2[3, 2];
+            m3[0, 3] = m1[0, 0] * m2[0, 3] + m1[0, 1] * m2[1, 3] + m1[0, 2] * m2[2, 3] + m1[0, 3] * m2[3, 3];
+            m3[1, 0] = m1[1, 0] * m2[0, 0] + m1[1, 1] * m2[1, 0] + m1[1, 2] * m2[2, 0] + m1[1, 3] * m2[3, 0];
+            m3[1, 1] = m1[1, 0] * m2[0, 1] + m1[1, 1] * m2[1, 1] + m1[1, 2] * m2[2, 1] + m1[1, 3] * m2[3, 1];
+            m3[1, 2] = m1[1, 0] * m2[0, 2] + m1[1, 1] * m2[1, 2] + m1[1, 2] * m2[2, 2] + m1[1, 3] * m2[3, 2];
+            m3[1, 3] = m1[1, 0] * m2[0, 3] + m1[1, 1] * m2[1, 3] + m1[1, 2] * m2[2, 3] + m1[1, 3] * m2[3, 3];
+            m3[2, 0] = m1[2, 0] * m2[0, 0] + m1[2, 1] * m2[1, 0] + m1[2, 2] * m2[2, 0] + m1[2, 3] * m2[3, 0];
+            m3[2, 1] = m1[2, 0] * m2[0, 1] + m1[2, 1] * m2[1, 1] + m1[2, 2] * m2[2, 1] + m1[2, 3] * m2[3, 1];
+            m3[2, 2] = m1[2, 0] * m2[0, 2] + m1[2, 1] * m2[1, 2] + m1[2, 2] * m2[2, 2] + m1[2, 3] * m2[3, 2];
+            m3[2, 3] = m1[2, 0] * m2[0, 3] + m1[2, 1] * m2[1, 3] + m1[2, 2] * m2[2, 3] + m1[2, 3] * m2[3, 3];
+            m3[3, 0] = m1[3, 0] * m2[0, 0] + m1[3, 1] * m2[1, 0] + m1[3, 2] * m2[2, 0] + m1[3, 3] * m2[3, 0];
+            m3[3, 1] = m1[3, 0] * m2[0, 1] + m1[3, 1] * m2[1, 1] + m1[3, 2] * m2[2, 1] + m1[3, 3] * m2[3, 1];
+            m3[3, 2] = m1[3, 0] * m2[0, 2] + m1[3, 1] * m2[1, 2] + m1[3, 2] * m2[2, 2] + m1[3, 3] * m2[3, 2];
+            m3[3, 3] = m1[3, 0] * m2[0, 3] + m1[3, 1] * m2[1, 3] + m1[3, 2] * m2[2, 3] + m1[3, 3] * m2[3, 3];
+            return new Matrix4x4(m3);
+        }
+
+        public static Matrix4x4 operator *(Matrix4x4 m, float scalar)
+        {
+            return new Matrix4x4(Multiply(m, scalar));
+        }
+    }
+}
---
+++ Assets/MathsUtility/Vector3.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MathsPhys
+{
+	[System.Serializable]
+    public class Vector3
+    {
+        public static Vector3 Zero = NewZero();
+		public static Vector3 One = NewOne();
+		public static Vector3 Up = new Vector3(0, 1, 0);
+		public static Vector3 Left = new Vector3(1, 0, 0);
+		public static Vector3 forward = new Vector3(0, 0, 1);
+
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3()
+        {
+            x = 0.0f;
+            y = 0.0f;
+            z = 0.0f;
+        }
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector3(float xyz)
+        {
+            this.x = xyz;
+            this.y = xyz;
+            this.z = xyz;
+        }
+
+        public Vector3(Vector3 v)
+        {
+            this.x = v.x;
+            this.y = v.y;
+            this.z = v.z;
+        }
+
+        public static Vector3 NewZero()
+        {
+            return new Vector3(0.0f);
+        }
+
+        public static Vector3 NewOne()
+        {
+            return new Vector3(1.0f);
+        }
+
+        public float DotProduct(Vector3 other)
+        {
+            return x * other.x + y * other.y + z * other.z;
+        }
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {           
+            return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+
+        public static Vector3 operator -(Vector3 v)
+        {
+            return new Vector3(-v.x, -v.y, -v.z);
+        }
+
+        public static Vector3 operator *(Vector3 v, float scalar)
+        {
+            return new Vector3(v.x * scalar, v.y * scalar, v.z * scalar);
+        }
+
+		public static Vector3 operator *(Vector3 v1, Vector3 v2)
+		{
+			return new Vector3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+		}
+
+        public static Vector3 operator /(Vector3 v, float scalar)
+        {
+            return new Vector3(v.x / scalar, v.y / scalar, v.z / scalar);
+        }
+
+        public static bool operator ==(Vector3 v1, Vector3 v2)
+        {
+            return v1.x == v2.x && v1.y == v2.y && v1.z == v2.z;
+        }
+
+        public static bool operator !=(Vector3 v1, Vector3 v2)
+        {
+            return v1.x != v2.x || v1.y != v2.y || v1.z != v2.z;
+        }
+
+        // Cast from MathsPhys Vect3 to Unity Vect3 
+        public static implicit operator UnityEngine.Vector3(Vector3 vector)
+        {
+            return new UnityEngine.Vector3(vector.x, vector.y, vector.z);
+        }
+
+        // Cast from Unity Vect3 to MathsPhys Vect3
+        public static implicit operator Vector3(UnityEngine.Vector3 vector)
+        {
+            return new Vector3(vector.x, vector.y, vector.z);
+        }
+
+        public static Vector3 CrossProduct(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+        }
+
+        public Vector3 Add(Vector3 v)
+        {
+            x += v.x;
+            y += v.y;
+            z += v.z;
+            return this;
+        }
+
+        public float DistanceTo(Vector3 v)
+        {
+            float dx = this.x - v.x;
+            float dy = this.y - v.y;
+            float dz = this.z - v.z;
+            return (float)Mathf.Sqrt(dx * dx + dy * dy + dz * dz);
+        }
+
+        public float Size()
+        {
+            return DistanceTo(Vector3.Zero);
+        }
+
+        public Vector3 Normalize()
+        {
+            float size = Size();
+            this.x /= size;
+            this.y /= size;
+            this.z /= size;
+            return this;
+        }
+
+		public float Magnitude()
+		{
+			return Mathf.Sqrt (x*x + y*y + z*z);
+		}
+
+        public Vector3 Clone()
+        {
+            return new Vector3(this);
+        }
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ", " + z + ")";
+        }
+    }
+}
